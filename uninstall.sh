4eb4a757809242abe0ddc5d4c6c88127fee627da
Emil Pogi <markemil.dacoylo13@gmail.com>
2024-07-05 11:22:04 +0800

    Fix

--- uninstall.sh
+++ uninstall.sh
@@ -5,5 +5,8 @@ sudo systemctl stop nginx
 yes | sudo apt remove --purge nginx
 yes | sudo apt autoremove
 sudo systemctl stop mysql
-yes | sudo apt remove --purge mysql-server mysql-client
+yes | sudo apt remove --purge mysql-server mysql-client mysql-common
 yes | sudo apt autoremove
+
+yes | sudo apt clean
+yes | sudo apt update
