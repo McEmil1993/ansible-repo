8737743a0ce48cac8a233c7da108fc83350030e4
Emil Pogi <markemil.dacoylo13@gmail.com>
2024-07-05 12:42:11 +0800

    Fix

--- ansible/roles/mysql/files/create_mysql_user.sh
+++ ansible/roles/mysql/files/create_mysql_user.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Define MySQL credentials and user details
+
 MYSQL_USERNAME="manager"
 MYSQL_USER_PASSWORD="12345"
-MYSQL_ROOT_PASSWORD="  "  # Replace with your actual MySQL root password
+MYSQL_ROOT_PASSWORD="  "  
 
-# MySQL commands to create user and grant privileges
+
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<MYSQL_SCRIPT
 CREATE USER '${MYSQL_USERNAME}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USERNAME}'@'%';
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
-# Optionally, you can output success/failure messages or handle errors
+
 if [ $? -eq 0 ]; then
     echo "MySQL user '${MYSQL_USERNAME}' created successfully."
 else
